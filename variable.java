d9bec096ffa3da2592b40c9eb32588f3169d39f9
Pallavi <61791255+Pallavigoyal02@users.noreply.github.com>
2020-08-22 16:11:31 +0530

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,17 @@
+package com.java_program;
+
+public class Datatype{
+
+	public static void main(String[] args) {
+		int myNum = 20;
+		float myFloatNum = 20.25f;
+		char myLetter = 'P';
+		boolean myBool = true;
+		String myText = "Unknown";
+		System.out.print("myNum");
+		System.out.print("myFloatNum");
+		System.out.print("myLetter");
+		System.out.print("myBool");
+		System.out.print("myText");
+	}
+}
